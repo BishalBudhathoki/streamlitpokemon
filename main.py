48a6d6f8c5ec22070d3d85702448d08841d64eb2
Bishal Budhathoki <35174206+BishalBudhathoki@users.noreply.github.com>
2024-02-11 10:50:30 +0000

    got pokemon data and image and is displaying

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import requests
+import json
 
 st.title('Pokemon Data Visualiser tool')
 st.divider()
 
 
-#Load Data
-@st.cache_data
-def load_data():
-    data = pd.read_csv('pokemon.csv')
+# #Load Data
+# @st.cache_data
+# def load_data():
+#     data = pd.read_csv('pokemon.csv')
+#     return data
+
+# data = load_data()
+
+# #create a dataframe with pokemon data
+# df = pd.DataFrame(data)
+
+# #display the data
+# st.dataframe(df)
+
+def get_pokemon_data() -> dict:
+    try:
+        url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+        response = requests.get(url)
+        data = response.json()
+    except Exception as e:
+        st.write(f'Error: {e}')
+        data = None
     return data
 
-data = load_data()
+pokemon_data = get_pokemon_data()
 
-#create a dataframe with pokemon data
-df = pd.DataFrame(data)
+if pokemon_data:
+    st.header(pokemon_data.get('name').capitalize())
+    st.subheader(pokemon_data.get('weight'))
+    st.image(pokemon_data.get('sprites').get('front_default'))
 
-#display the data
-st.write(df)
+
+
