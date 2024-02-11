ba24ea70bc08cb09a2171bfaa4b73a39346fff2b
Bishal Budhathoki <35174206+BishalBudhathoki@users.noreply.github.com>
2024-02-11 14:11:00 +0000

    2 different pokeman generated and arranged in column

--- main.py
+++ main.py
@@ -29,24 +29,38 @@ colours = {
 st.title('Pokemon Data Visualiser tool')
 st.divider()
 
-
-# #Load Data
-# @st.cache_data
-# def load_data():
-#     data = pd.read_csv('pokemon.csv')
-#     return data
-
-# data = load_data()
-
-# #create a dataframe with pokemon data
-# df = pd.DataFrame(data)
-
-# #display the data
-# st.dataframe(df)
-
-def get_pokemon_data() -> dict:
+# get a random pokedex number
+random_number = str(np.random.randint(1, 151))
+random_number_2 = str(np.random.randint(1, 151))
+st.write(random_number)
+# fetch name of that random pokemon
+def get_random_pokemon_name() -> dict:
     try:
-        url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+        url = 'https://pokeapi.co/api/v2/pokemon-species/'+random_number
+        response = requests.get(url)
+        data = response.json()
+        # st.write(data)
+    except Exception as e:
+        st.write(f'Error: {e}')
+        data = None
+    return data.get('name')
+
+# fetch name of that random pokemon
+def get_random_pokemon_name_2() -> dict:
+    try:
+        url = 'https://pokeapi.co/api/v2/pokemon-species/'+random_number_2
+        response = requests.get(url)
+        data = response.json()
+        # st.write(data)
+    except Exception as e:
+        st.write(f'Error: {e}')
+        data = None
+    return data.get('name')
+
+
+def get_pokemon_data(pokemon_name) -> dict:
+    try:
+        url = 'https://pokeapi.co/api/v2/pokemon/'+pokemon_name
         response = requests.get(url)
         data = response.json()
     except Exception as e:
@@ -54,16 +68,32 @@ def get_pokemon_data() -> dict:
         data = None
     return data
 
-pokemon_data = get_pokemon_data()
+pokemon_name = get_random_pokemon_name()
+pokemon_name_2 = get_random_pokemon_name_2()
 
-if pokemon_data:
-    st.header(pokemon_data.get('name').capitalize())
-    st.image(pokemon_data.get('sprites').get('front_default'))
-    st.write('Pokemon weight:', pokemon_data.get('weight'))
-    pokemon_type = pokemon_data.get('types')[0].get('type').get('name')
-    annotated_text(
-        (f'Pokemon Type: {pokemon_type}',"", colours[pokemon_type]),
-    )
+pokemon_data = get_pokemon_data(pokemon_name)
+pokemon_data_2 = get_pokemon_data(pokemon_name_2)
+
+if pokemon_data and pokemon_data_2:
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header(pokemon_data.get('name').capitalize())
+        st.image(pokemon_data.get('sprites').get('front_default'))
+        st.write('Pokemon weight:', pokemon_data.get('weight'))
+        pokemon_type = pokemon_data.get('types')[0].get('type').get('name')
+        annotated_text(
+            (f'Pokemon Type: {pokemon_type}',"", colours[pokemon_type]),
+        )
+
+    with col2:
+        st.header(pokemon_data_2.get('name').capitalize())
+        st.image(pokemon_data_2.get('sprites').get('front_default'))
+        st.write('Pokemon weight:', pokemon_data_2.get('weight'))
+        pokemon_type = pokemon_data_2.get('types')[0].get('type').get('name')
+        annotated_text(
+            (f'Pokemon Type: {pokemon_type}',"", colours[pokemon_type]),
+        )
     
 
 
